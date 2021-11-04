c11b2720286b39b890362e055723ecf3f70f4c4d
Richard Hess <eswat2@gmail.com>
2021-11-04 11:21:15 -0700

    bump 0.0.3

--- src/components/Proofs.svelte
+++ src/components/Proofs.svelte
@@ -1,79 +1,10 @@
 <script>
-  import Toggle from "./Toggle.svelte"
-  import { navy } from "../utils"
+  import Toggle from './Toggle.svelte';
+  import { navy } from '../utils';
 
-  export let size = undefined
-
-  const Keys = [
-    "acura",
-    "alfa-romeo",
-    "am-general",
-    "aston-martin",
-    "audi",
-    "bently",
-    "bmw",
-    "bugatti",
-    "buick",
-    "cadillac",
-    "chevrolet",
-    "chrysler",
-    "citroen",
-    "daewoo",
-    "dodge",
-    "eagle",
-    "ferrari",
-    "fiat",
-    "fisker",
-    "ford",
-    "genesis",
-    "geo",
-    "gmc",
-    "honda",
-    "hummer",
-    "hyundai",
-    "infiniti",
-    "isuzu",
-    "jaguar",
-    "jeep",
-    "kia",
-    "lamborghini",
-    "land-rover",
-    "lexus",
-    "lincoln",
-    "lotus",
-    "maserati",
-    "maybach",
-    "mazda",
-    "mclaren",
-    "mercedes-benz",
-    "mercury",
-    "mini",
-    "mitsubishi",
-    "nissan",
-    "oldsmobile",
-    "opel",
-    "panoz",
-    "peugeot",
-    "plymouth",
-    "pontiac",
-    "porsche",
-    "ram",
-    "renault",
-    "rolls-royce",
-    "saab",
-    "saturn",
-    "scion",
-    "seat",
-    "skoda",
-    "smart",
-    "spyder",
-    "subaru",
-    "suzuki",
-    "tesla",
-    "toyota",
-    "volkswagen",
-    "volvo",
-  ]
+  export let size = undefined;
+  // NOTE:  this is loaded on the toplevel page... [ see index.html ]
+  const Keys = window.protoIkons.ikons;
 </script>
 
 <div class="proofs">
